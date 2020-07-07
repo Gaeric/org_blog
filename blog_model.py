b16800a8a38e1b9d63aa4bdb8fc7826735ad29fb
Gaeric <1062217098@qq.com>
2020-07-07 22:16:05 +0800

    增加summary和更新时间及字数

--- blog_model.py
+++ blog_model.py
@@ -6,18 +6,20 @@
 # Author:   Gaeric
 
 import re
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 title_re = re.compile(r'<title>([\s\S]*)<\/title>')
 body_re = re.compile(r'<body>([\s\S]*)<\/body>')
+summary_re = re.compile(r'<li><a href="#(.*)">(.*?)简介<\/a><\/li>')
 
 
 class OrgBlog():
     """解析传入的ox-html文件，匹配其title和body
     文件格式必须为html，由调用者保证
     如果title不存在，则返回""
-    如果body不存在，则返回ox-html文件的所有内容
-"""
+    如果body不存在，则返回ox-html文件的所有内容"""
 
     def __init__(self, oxhtml):
         with open(oxhtml, 'r', encoding='utf-8') as fp:
@@ -26,9 +28,33 @@ class OrgBlog():
             except IOError as ioerr:
                 raise f"Read orghtml Failed, Error: {ioerr}"
             else:
+                self._html_context = html_context
                 title_ma = title_re.search(html_context)
                 body_ma = body_re.search(html_context)
+                summary_ma = summary_re.search(html_context)
             finally:
                 self.org_title = title_ma.group(1) if title_ma else ""
                 self.org_content = body_ma.group(1) \
                     if body_ma else html_context
+                self._summary_id = "outline-container-" + \
+                    summary_ma.group(1) if summary_ma else ""
+
+    @property
+    def org_summary(self):
+        if self._summary_id == "":
+            return ""
+        bs = BeautifulSoup(self._html_context, "lxml")
+        attrs = {'id': self._summary_id}
+        su = bs.find("div", attrs=attrs).find('p')
+        return su.string or ""
+
+    @property
+    def org_createtime(self):
+        bs = BeautifulSoup(self._html_context, "lxml")
+        attrs = {'class': "date"}
+        createtime_s = bs.find("p", attrs=attrs)
+        if createtime_s is None:
+            return 0
+        createtime = datetime.strptime(createtime_s.string,
+                                       "Created: %Y-%m-%d %a %M:%S")
+        return createtime
--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from flask import Flask
 from flask import render_template
 from flask import g
 from blog_model import OrgBlog
+from datetime import datetime
 import os
 import config
 
@@ -34,6 +35,60 @@ def get_static_title(ox_html):
     file_path = os.path.join(os.getcwd(), "static", config.BLOG_DIR, ox_html)
     title = OrgBlog(file_path).org_title
     return title
+
+
+@app.template_filter('html_summary')
+def get_static_summary(ox_html):
+    """函数以启动文件为根目录，ox_html为其在static/static_html/下的相对位置"""
+    file_path = os.path.join(os.getcwd(), 'static', config.BLOG_DIR, ox_html)
+    summary = OrgBlog(file_path).org_summary
+    return summary
+
+
+@app.template_filter('html_content')
+def get_static_content(ox_html):
+    """函数以启动文件为根目录，ox_html为其在static/static_html/下的相对位置"""
+    file_path = os.path.join(os.getcwd(), "static", config.BLOG_DIR, ox_html)
+    content = OrgBlog(file_path).org_content
+    return content
+
+
+# datetime.strptime("2020-07-05 Sun 20:34", "%Y-%m-%d %a %M:%S")
+@app.template_filter('html_time')
+def html_time(ox_html):
+    file_path = os.path.join(os.getcwd(), 'static', config.BLOG_DIR, ox_html)
+    createtime = OrgBlog(file_path).org_createtime
+    return handle_time(createtime)
+
+
+def handle_time(time):
+    """
+    time 距离现在的时间：
+    1. 如果时间小于1分钟，则显示刚刚
+    2. 如果时间小于1小时，则显示×××分钟前
+    3. 如果时间小于24小时，则显示×××小时前
+    4. 如果时间小于24*30小时，则显示×××天前
+    5. 否则，则显示具体的日期（格式为××××年××月××日）
+    """
+    if isinstance(time, datetime):
+        current_time = datetime.now()
+        timestamp = (current_time - time).total_seconds()
+        if timestamp < 60:
+            return '刚刚'
+        elif timestamp >= 60 and timestamp < 3600:
+            minutes = timestamp / 60
+            return f'{int(minutes)}分钟前'
+        elif timestamp >= 3600 and timestamp < 86400:
+            hours = timestamp / 3600
+            return f'{int(hours)}小时前'
+        elif timestamp >= 86400 and timestamp < 2592000:
+            days = timestamp / 86400
+            return f'{int(days)}天前'
+        else:
+            return time.strftime('%Y年%m月%d日')
+        pass
+    else:
+        return time
 
 
 @app.route('/')
